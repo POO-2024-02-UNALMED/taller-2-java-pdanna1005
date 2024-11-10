4a2e1a8427a77781cb5ecfae88be25bb377620c8
pdanna1005 <pdanna1005@gmail.com>
2024-11-10 11:53:30 -0500

    Update TestMetodos.java

--- src/test/java/test/TestMetodos.java
+++ src/test/java/test/TestMetodos.java
@@ -6,10 +6,6 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-import Asiento;
-import Auto;
-import Motor;
-
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class TestMetodos {
 
